4731a53d118df82353798f7602de9d9a64afddec
zeroxoxo <zeroxoxo888@gmail.com>
2017-12-06 17:28:48 +0700

    test homology

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,12 +1,16 @@
 import pandas as pd
-def test_homology_file_duplicate(file):
+def test_homology_file(file):
     homology = pd.read_table(file, header=None, sep='\t')
     rows, cols = homology.shape
     for i in range(cols):
-        f = homology.iloc[:, i].duplicated(keep=False)
-        for j in f:
+        f = homology.iloc[:, i]
+        g = f.duplicated(keep=False)
+        for j in g:
             if j == True:
+                return print('Oops')
+        for k in f:
+            if not k:
                 return print('Oops')
     return print("It's okay, man")
 
-test_homology_file_duplicate('~/coursepaper/output/temp/clean/0_homology_genes.tsv')
+test_homology_file('~/coursepaper/output/temp/clean/0_homology_genes.tsv')
