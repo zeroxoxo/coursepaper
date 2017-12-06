e992552d8a19daf81fb0660c0da8755908e424b4
zeroxoxo <zeroxoxo888@gmail.com>
2017-12-06 16:50:43 +0700

    vb_import -> clean.py -> clean_g.py make work

--- Clean_g.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-def clean_g(path):
-    ref = pd.read_table(path + '0_homology_genes.tsv', sep='\t')
-    rows, col = ref.shape()
-    for i in range(col):
-        j = pd.read_table(path + str(i) + '_gene.tsv', header=False, sep='\t')
-        g = j.loc[j[j.columns[4]].isin(ref[ref.columns[i]])]]
-        g.to_csv(path + str(i) + '_clean_genes.tsv', sep='\t')
