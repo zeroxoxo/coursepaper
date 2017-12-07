50c79832ab0015632e484c6b82912378e3f96724
zeroxoxo <zeroxoxo888@gmail.com>
2017-12-07 14:38:06 +0700

    Full homology cleansing: empty cells as well as duplicates

--- Scripts/Clean1.py
+++ Scripts/Clean1.py
@@ -1,10 +1,10 @@
 import pandas as pd
 def clean(input_file, output_file):
-    in_file = pd.read_table(input_file, sep='\t')
+    in_file = pd.read_table(input_file, sep='\t').dropna()
     rows, cols = in_file.shape
     for i in range(cols):
         in_file = in_file.drop_duplicates(subset=in_file.iloc[:,[i]], keep=False)
     in_file.to_csv(output_file, sep='\t', index=False)
     return print('Done')
 
-clean('/home/lab51/coursepaper/output/temp/clean/1_homology_genes.tsv', '/home/lab51/coursepaper/output/temp/clean/0_homology_genes.tsv')
+clean('/home/lab51/coursepaper/output/temp/imp/0_homology_genes.tsv', '/home/lab51/coursepaper/output/temp/clean/0_homology_genes.tsv')
