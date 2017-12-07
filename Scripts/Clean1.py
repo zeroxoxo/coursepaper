611cdb0070d69318ac9747338dda09a0dbb07f44
zeroxoxo <zeroxoxo888@gmail.com>
2017-12-07 13:35:04 +0700

    duplicate cleaning of homology file after clean.py cleansing

---
+++ Scripts/Clean1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+def clean(input_file, output_file):
+    in_file = pd.read_table(input_file, sep='\t')
+    rows, cols = in_file.shape
+    for i in range(cols):
+        in_file = in_file.drop_duplicates(subset=in_file.iloc[:,[i]], keep=False)
+    in_file.to_csv(output_file, sep='\t', index=False)
+    return print('Done')
+
+clean('/home/lab51/coursepaper/output/temp/clean/2_homology_genes.tsv', '/home/lab51/coursepaper/output/temp/clean/3_homology_genes.tsv')
