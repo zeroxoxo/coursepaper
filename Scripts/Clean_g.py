c07c7276d207cfe620610c248a550ac8fae11636
zeroxoxo <zeroxoxo888@gmail.com>
2017-12-08 16:05:11 +0700

    small changes

--- Scripts/Clean_g.py
+++ Scripts/Clean_g.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import click
 def clean_g(h_path, g_path):
     ref = pd.read_table(h_path + '0_homology_genes.tsv', sep='\t')
     rows, col = ref.shape
@@ -6,6 +7,6 @@ def clean_g(h_path, g_path):
         j = pd.read_table(g_path + str(i) + '_gene.tsv', header=None, sep='\t')
         g = j.loc[j[j.columns[4]].isin(ref[ref.columns[i]])]
         g.to_csv(h_path + str(i) + '_clean_genes.tsv', sep='\t', header=False, index=False)
-    return print('Done')
+    return print('Done Clean_g')
 
-clean_g('~/coursepaper/Data/temp/clean/', '~/coursepaper/Data/temp/imp/')
+#clean_g('~/coursepaper/Data/temp/clean/', '~/coursepaper/Data/temp/imp/')
--- Scripts/Clean_h.py
+++ Scripts/Clean_h.py
@@ -1,10 +1,11 @@
 import pandas as pd
-def clean(input_file, output_file):
+import click
+def clean_h(input_file, output_file):
     in_file = pd.read_table(input_file, sep='\t').dropna()
     rows, cols = in_file.shape
     for i in range(cols):
         in_file = in_file.drop_duplicates(subset=in_file.iloc[:,[i]], keep=False)
     in_file.to_csv(output_file, sep='\t', index=False)
-    return print('Done')
+    return print('Done Clean_h.')
 
-clean('/home/lab51/coursepaper/Data/temp/imp/0_homology_genes.tsv', '/home/lab51/coursepaper/Data/temp/clean/0_homology_genes.tsv')
+#clean_h('/home/lab51/coursepaper/Data/temp/imp/0_homology_genes.tsv', '/home/lab51/coursepaper/Data/temp/clean/0_homology_genes.tsv')
